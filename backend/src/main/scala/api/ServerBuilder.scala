3aed6205a83970db9d67827fb83a56d9d9300ffd
pploszczyca <piotrus.plo@gmail.com>
2022-04-08 21:00:56 +0200

    Made host and port constants

--- backend/src/main/scala/api/ServerBuilder.scala
+++ backend/src/main/scala/api/ServerBuilder.scala
@@ -1,15 +1,15 @@
 package api
 
 import cats.effect.{ExitCode, IO}
-import com.comcast.ip4s.{ipv4, port}
+import constants.Constants.{HOST_IP, PORT}
 import org.http4s.ember.server.EmberServerBuilder
 
 class ServerBuilder(elevatorService: ElevatorService):
   def apply(): IO[ExitCode] = {
     EmberServerBuilder
       .default[IO]
-      .withHost(ipv4"127.0.0.1")
-      .withPort(port"8080")
+      .withHost(HOST_IP)
+      .withPort(PORT)
       .withHttpApp(elevatorService.routes)
       .build
       .use(_ => IO.never)
