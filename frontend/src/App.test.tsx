f2df93e8e6586745c06bee181a056e0820ee812e
pploszczyca <piotrus.plo@gmail.com>
2022-04-09 14:54:31 +0200

    Remove old frontend test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText("Elevator ID");
-  expect(linkElement).toBeInTheDocument();
+test('Empty test', () => {
+
 });
